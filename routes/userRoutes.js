625371756feb9708c3f1c39d10b716856e9b1f6b
xCubeArrow <1984flaker@gmail.com>
2020-02-26 22:03:38 +0100

    Add new routes for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const User = require("../modules/user")
 
 const router = express.Router();
+
+// Get all users
 router.get("/", async(req, res) => {
     try {
         const Users = await User.find();
@@ -11,4 +14,31 @@ router.get("/", async(req, res) => {
     }
 });
 
+// Get the user from the userId
+router.get("/:userId", async(req, res) => {
+    try{
+        const user = await User.findById(new mongoose.Types.ObjectId(req.params.userId))
+        
+        res.json(user)
+    }
+    catch (e){
+        res.json({message: e})
+    }
+})
+
+// Post a new user
+// Returns the newly generated user
+router.post("/", async(req, res) => {
+    try{
+        const user = new User(req.body)
+        user._id = mongoose.Types.ObjectId()
+        user.save((err, user) => {
+            if (err) res.json({message: err})
+            res.json(user)
+        })
+    }
+    catch (e){
+        res.json({message: e})
+    }
+})
 module.exports = router;
