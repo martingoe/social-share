a572a4a7e8d84c89698dd3388926eab0319822b1
xCubeArrow <1984flaker@gmail.com>
2020-02-27 20:53:32 +0100

    Add more routes for userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose")
+const hashingUtils = require("../utils/hashig")
 const User = require("../modules/user")
 
 const router = express.Router();
@@ -31,6 +32,7 @@ router.get("/:userId", async(req, res) => {
 router.post("/", async(req, res) => {
     try{
         const user = new User(req.body)
+        user.password = hashingUtils.hashFunc(user.password)
         user._id = mongoose.Types.ObjectId()
         user.save((err, user) => {
             if (err) res.json({message: err})
@@ -41,4 +43,33 @@ router.post("/", async(req, res) => {
         res.json({message: e})
     }
 })
+
+router.post("/login", async(req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email})
+        if(hashingUtils.checkPassword(req.body.password, user.password)){
+            res.cookie("user", user)
+            res.json(user)
+        } else{
+            res.status(403).json({message: "The email or password was incorrect"})
+        }
+    } catch (e){
+        res.json({message: e})
+    }
+})
+
+router.delete("/:userId", async(req, res) => {
+    try{
+        const objectId = new mongoose.Types.ObjectId(req.params.userId)
+        User.findByIdAndRemove({_id: objectId}, function(err){
+            if (err) 
+                res.json({message: err})
+            res.json({message: "Deleted the user"})
+        })
+        
+    } catch (e){
+        res.json({message: e})
+    }
+})
+
 module.exports = router;
