d013c243febf88ce44277a25dcd614704c912fe8
xCubeArrow <1984flaker@gmail.com>
2020-02-28 16:49:55 +0100

    Add an 404 error

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 app.use(cookieParser())
 app.use("/api/user", userRoutes);
 
+// Catch 404 errors
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to " + process.env.MONGODB_CONNECTION)
