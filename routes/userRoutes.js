57c3f52139fdc7525eb27892f4e89a983cbb9519
xCubeArrow <1984flaker@gmail.com>
2020-02-28 16:06:10 +0100

    Remove unnecessary dependency

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const hashingUtils = require("../utils/hashig")
-const session = require("express-session")
 const User = require("../modules/user")
 const sidUtils = require("../utils/idCreator")
 
