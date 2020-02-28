5d180c299eaa297292cc100309cab4693138c6c3
xCubeArrow <1984flaker@gmail.com>
2020-02-28 16:03:49 +0100

    Add a sid to the user schema, Remove args from _id

--- modules/user.js
+++ modules/user.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-    _id: {type: mongoose.Types.ObjectId, unique:true, required:true},
+    _id: {type: mongoose.Types.ObjectId},
     username: {type: String, unique: true, required:true},
     email: {type: String, unique:true, required:true},
     password: {type: String, required:true},
     phone: String,
+    sid: String,
     name: {
         forename: String,
         surname: String
