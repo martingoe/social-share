2a682e099e2ad58620887323a70a5f9def9ba415
xCubeArrow <1984flaker@gmail.com>
2020-03-03 21:16:47 +0100

    Add new transform
    The default transform to posts.js

--- utils/transform.js
+++ utils/transform.js
@@ -6,8 +6,17 @@ const userDefaultPrint = (doc, ret, options) => {
     delete ret.phone
     delete ret.__v
     delete ret.name
+    delete ret.followers
 
     return ret
 }
 
-module.exports = {userDefaultPrint}
+const postDefaultPrint = (doc, ret, options) => {
+    delete ret.likesUsers
+    delete ret.dislikesUsers
+    delete ret.__v
+    ret.creationDate = ret.creationDate.toLocaleString()
+
+    return ret
+}
+module.exports = {userDefaultPrint, postDefaultPrint}
