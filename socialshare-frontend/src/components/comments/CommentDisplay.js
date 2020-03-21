c13d8aa0de1cb12899172eb13ab85972f2672a67
xCubeArrow <1984flaker@gmail.com>
2020-03-21 23:22:14 +0100

    Add a simple comment functionality
    This currently only displays the content but a start

---
+++ socialshare-frontend/src/components/comments/CommentDisplay.js
@@ -0,0 +1,46 @@
+import React from "react"
+import Comment from "./Comment"
+
+export default class CommentDisplay extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoaded: false,
+          comments: [],
+          error: null
+        };
+      }
+
+    componentDidMount() {
+        fetch(this.props.url, {
+            method: "GET",
+            credentials: "include",
+            mode: "cors"
+        }).then(response => response = response.json())
+        .then((result) => {
+            if(this.message != null){
+                this.setState({error: "The requested post does not exist"})
+            }
+            this.setState({
+                isLoaded: true, 
+                comments: result
+            })
+        })
+    }
+
+    render(){
+        if(!this.state.isLoaded){
+            return <div style={{paddingTop: "100px"}}><div className="spinner"/> <p>Loading ...</p></div>
+        } else if (this.state.error){
+            return <div style={{paddingTop: "100px"}}><h1>The requested post could not be found</h1></div>
+        } else {
+            return (
+                <div>
+                {this.state.comments.map(comment => (
+                    <Comment value={comment} key={comment._id}></Comment>
+                ))}
+                </div>
+            )
+        }
+    }
+}
